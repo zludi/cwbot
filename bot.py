e7088fd8f15c22d4ad4e9541c741514ccafcb8a1
Горбунов Валерий <g.space@mail.ru>
2018-01-08 00:56:36 +0300

    second commit

---
+++ bot.py
@@ -0,0 +1,33 @@
+import telebot
+import json
+import urllib.request as ul
+
+TOKEN = '510281377:AAGrLekadolEI_9kjQn3TYbe6DMod_gXlgI'
+bot = telebot.TeleBot(TOKEN)
+country = None
+@bot.message_handler(commands=['start'])
+def start(start):
+    sent = bot.send_message(start.chat.id, 'Your country or state')
+    bot.register_next_step_handler(sent, town)
+def town(countryRequest):
+    global country
+    sent = bot.send_message(countryRequest.chat.id, 'Your city')
+    country = countryRequest
+    bot.register_next_step_handler(sent, weather)
+def weather(townRequest):
+    url = 'http://api.wunderground.com/api/17fd104bf040ac3b/geolookup/conditions/q/'+ country.text + '/' + str(townRequest.text) + '.json'
+    f = ul.urlopen(url)
+    json_string = f.read()
+    parsed_json = json.loads(json_string)
+    location = parsed_json['location']['city']
+    temp_c = parsed_json['current_observation']['temp_c']
+    date = parsed_json['current_observation']['observation_time']
+    wind =  parsed_json['current_observation']['wind_dir']
+    windSpeed  =  parsed_json['current_observation']['wind_kph']
+    pressure = int( parsed_json['current_observation']['pressure_mb']) / 1000 * 750 
+    UV =  parsed_json['current_observation']['UV']
+    cloudness =  parsed_json['current_observation']['weather']
+    humidity = parsed_json['current_observation']['relative_humidity']
+    bot.send_message(townRequest.chat.id, '\n'.join(f'Temperature in {location}: {temp_c} °C; Cloudness: {cloudness}; Humidity: {humidity}; Wind {wind}, {windSpeed} kph; Pressure: {pressure} mm Hg; UV: {UV}; {date}'.split('; '))) 
+    f.close()	
+bot.polling() 
